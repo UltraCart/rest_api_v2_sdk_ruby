e9043beeadf5bb493e74b9d5ba133e32ea384e4b
Perry Tew <perry@ultracart.com>
2021-07-13 10:51:17 -0400

    storefront communications - added methods to retrieve step dispatch logs

---
+++ docs/EmailCommseqStepLog.md
@@ -0,0 +1,10 @@
+# UltracartClient::EmailCommseqStepLog
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**email** | **String** | Email | [optional] 
+**log** | **String** | Log text | [optional] 
+**log_dts** | **String** | Log date/time | [optional] 
+
+
--- lib/ultracart_api/api/storefront_api.rb
+++ lib/ultracart_api/api/storefront_api.rb
@@ -2596,6 +2596,71 @@ module UltracartClient
       end
       return data, status_code, headers
     end
+    # Get email dispatch logs
+    # @param storefront_oid 
+    # @param commseq_uuid 
+    # @param commseq_step_uuid 
+    # @param [Hash] opts the optional parameters
+    # @return [EmailCommseqStepLogsResponse]
+    def get_email_dispatch_logs(storefront_oid, commseq_uuid, commseq_step_uuid, opts = {})
+      data, _status_code, _headers = get_email_dispatch_logs_with_http_info(storefront_oid, commseq_uuid, commseq_step_uuid, opts)
+      data
+    end
+
+    # Get email dispatch logs
+    # @param storefront_oid 
+    # @param commseq_uuid 
+    # @param commseq_step_uuid 
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(EmailCommseqStepLogsResponse, Fixnum, Hash)>] EmailCommseqStepLogsResponse data, response status code and response headers
+    def get_email_dispatch_logs_with_http_info(storefront_oid, commseq_uuid, commseq_step_uuid, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: StorefrontApi.get_email_dispatch_logs ...'
+      end
+      # verify the required parameter 'storefront_oid' is set
+      if @api_client.config.client_side_validation && storefront_oid.nil?
+        fail ArgumentError, "Missing the required parameter 'storefront_oid' when calling StorefrontApi.get_email_dispatch_logs"
+      end
+      # verify the required parameter 'commseq_uuid' is set
+      if @api_client.config.client_side_validation && commseq_uuid.nil?
+        fail ArgumentError, "Missing the required parameter 'commseq_uuid' when calling StorefrontApi.get_email_dispatch_logs"
+      end
+      # verify the required parameter 'commseq_step_uuid' is set
+      if @api_client.config.client_side_validation && commseq_step_uuid.nil?
+        fail ArgumentError, "Missing the required parameter 'commseq_step_uuid' when calling StorefrontApi.get_email_dispatch_logs"
+      end
+      # resource path
+      local_var_path = '/storefront/{storefront_oid}/email/commseqs/{commseq_uuid}/steps/{commseq_step_uuid}/logs'.sub('{' + 'storefront_oid' + '}', storefront_oid.to_s).sub('{' + 'commseq_uuid' + '}', commseq_uuid.to_s).sub('{' + 'commseq_step_uuid' + '}', commseq_step_uuid.to_s)
+
+      # query parameters
+      query_params = {}
+
+      # header parameters
+      header_params = {}
+      header_params['X-UltraCart-Api-Version'] = @api_client.select_header_api_version()
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = {}
+
+      # http body (model)
+      post_body = nil
+      auth_names = ['ultraCartBrowserApiKey', 'ultraCartOauth', 'ultraCartSimpleApiKey']
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => 'EmailCommseqStepLogsResponse')
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: StorefrontApi#get_email_dispatch_logs\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
     # Get email email
     # @param storefront_oid 
     # @param commseq_email_uuid 
