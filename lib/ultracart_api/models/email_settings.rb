fa5521c5cceee667bb7c7e83068845422463cc04
Perry Tew <perry@ultracart.com>
2022-09-12 09:05:53 -0400

    automation test

--- lib/ultracart_api/models/email_settings.rb
+++ lib/ultracart_api/models/email_settings.rb
@@ -32,6 +32,9 @@ module UltracartClient
 
     attr_accessor :postcard_from_state
 
+    # True if the Reviews.io integration is configured
+    attr_accessor :reviews_io_configured
+
     attr_accessor :sms_esp_twilio_uuid
 
     attr_accessor :sms_phone_number
@@ -54,6 +57,7 @@ module UltracartClient
         :'postcard_from_name' => :'postcard_from_name',
         :'postcard_from_postal_code' => :'postcard_from_postal_code',
         :'postcard_from_state' => :'postcard_from_state',
+        :'reviews_io_configured' => :'reviews_io_configured',
         :'sms_esp_twilio_uuid' => :'sms_esp_twilio_uuid',
         :'sms_phone_number' => :'sms_phone_number',
         :'transactional_esp_domain_user' => :'transactional_esp_domain_user',
@@ -74,6 +78,7 @@ module UltracartClient
         :'postcard_from_name' => :'String',
         :'postcard_from_postal_code' => :'String',
         :'postcard_from_state' => :'String',
+        :'reviews_io_configured' => :'BOOLEAN',
         :'sms_esp_twilio_uuid' => :'String',
         :'sms_phone_number' => :'String',
         :'transactional_esp_domain_user' => :'String',
@@ -126,6 +131,10 @@ module UltracartClient
         self.postcard_from_state = attributes[:'postcard_from_state']
       end
 
+      if attributes.has_key?(:'reviews_io_configured')
+        self.reviews_io_configured = attributes[:'reviews_io_configured']
+      end
+
       if attributes.has_key?(:'sms_esp_twilio_uuid')
         self.sms_esp_twilio_uuid = attributes[:'sms_esp_twilio_uuid']
       end
@@ -174,6 +183,7 @@ module UltracartClient
           postcard_from_name == o.postcard_from_name &&
           postcard_from_postal_code == o.postcard_from_postal_code &&
           postcard_from_state == o.postcard_from_state &&
+          reviews_io_configured == o.reviews_io_configured &&
           sms_esp_twilio_uuid == o.sms_esp_twilio_uuid &&
           sms_phone_number == o.sms_phone_number &&
           transactional_esp_domain_user == o.transactional_esp_domain_user &&
@@ -190,7 +200,7 @@ module UltracartClient
     # Calculates hash code according to all attributes.
     # @return [Fixnum] Hash code
     def hash
-      [marketing_esp_domain_user, marketing_esp_domain_uuid, marketing_esp_friendly_name, postcard_from_address1, postcard_from_address2, postcard_from_city, postcard_from_name, postcard_from_postal_code, postcard_from_state, sms_esp_twilio_uuid, sms_phone_number, transactional_esp_domain_user, transactional_esp_domain_uuid, transactional_esp_friendly_name].hash
+      [marketing_esp_domain_user, marketing_esp_domain_uuid, marketing_esp_friendly_name, postcard_from_address1, postcard_from_address2, postcard_from_city, postcard_from_name, postcard_from_postal_code, postcard_from_state, reviews_io_configured, sms_esp_twilio_uuid, sms_phone_number, transactional_esp_domain_user, transactional_esp_domain_uuid, transactional_esp_friendly_name].hash
     end
 
     # Builds the object from hash
