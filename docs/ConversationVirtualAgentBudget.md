f1b37c51eaedc47850e9e16f9d5ed636b5d07218
Perry Tew <perry@ultracart.com>
2025-05-19 13:01:53 -0400

    conversations - virtual AI budgets

---
+++ docs/ConversationVirtualAgentBudget.md
@@ -0,0 +1,11 @@
+# UltracartClient::ConversationVirtualAgentBudget
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**budget_daily** | **Float** |  | [optional] 
+**budget_monthly** | **Float** |  | [optional] 
+**used_daily** | **Float** |  | [optional] 
+**used_monthly** | **Float** |  | [optional] 
+
+
--- lib/ultracart_api/api/conversation_api.rb
+++ lib/ultracart_api/api/conversation_api.rb
@@ -2851,6 +2851,55 @@ module UltracartClient
       end
       return data, status_code, headers
     end
+    # Get virtual agent budget
+    # Retrieve virtual agent budget 
+    # @param [Hash] opts the optional parameters
+    # @return [ConversationVirtualAgentBudgetResponse]
+    def get_virtual_agent_budget(opts = {})
+      data, _status_code, _headers = get_virtual_agent_budget_with_http_info(opts)
+      data
+    end
+
+    # Get virtual agent budget
+    # Retrieve virtual agent budget 
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(ConversationVirtualAgentBudgetResponse, Fixnum, Hash)>] ConversationVirtualAgentBudgetResponse data, response status code and response headers
+    def get_virtual_agent_budget_with_http_info(opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: ConversationApi.get_virtual_agent_budget ...'
+      end
+      # resource path
+      local_var_path = '/conversation/virtualagent/budget'
+
+      # query parameters
+      query_params = {}
+
+      # header parameters
+      header_params = {}
+      header_params['X-UltraCart-Api-Version'] = @api_client.select_header_api_version()
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = {}
+
+      # http body (model)
+      post_body = nil
+      auth_names = ['ultraCartOauth', 'ultraCartSimpleApiKey']
+      data, status_code, headers = @api_client.call_api(:GET, local_var_path,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => 'ConversationVirtualAgentBudgetResponse')
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: ConversationApi#get_virtual_agent_budget\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
     # Insert a canned message
     # Insert a canned message 
     # @param canned_message Canned message
@@ -4625,6 +4674,61 @@ module UltracartClient
         :return_type => 'ConversationPbxVoicemailMailboxResponse')
       if @api_client.config.debugging
         @api_client.config.logger.debug "API called: ConversationApi#update_pbx_voicemail_mailbox\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+    # Update virtual agent budget
+    # Update virtual agent budget 
+    # @param virtual_agent_budget Virtual Agent Budget
+    # @param [Hash] opts the optional parameters
+    # @return [ConversationVirtualAgentBudgetResponse]
+    def update_virtual_agent_budget(virtual_agent_budget, opts = {})
+      data, _status_code, _headers = update_virtual_agent_budget_with_http_info(virtual_agent_budget, opts)
+      data
+    end
+
+    # Update virtual agent budget
+    # Update virtual agent budget 
+    # @param virtual_agent_budget Virtual Agent Budget
+    # @param [Hash] opts the optional parameters
+    # @return [Array<(ConversationVirtualAgentBudgetResponse, Fixnum, Hash)>] ConversationVirtualAgentBudgetResponse data, response status code and response headers
+    def update_virtual_agent_budget_with_http_info(virtual_agent_budget, opts = {})
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: ConversationApi.update_virtual_agent_budget ...'
+      end
+      # verify the required parameter 'virtual_agent_budget' is set
+      if @api_client.config.client_side_validation && virtual_agent_budget.nil?
+        fail ArgumentError, "Missing the required parameter 'virtual_agent_budget' when calling ConversationApi.update_virtual_agent_budget"
+      end
+      # resource path
+      local_var_path = '/conversation/virtualagent/budget'
+
+      # query parameters
+      query_params = {}
+
+      # header parameters
+      header_params = {}
+      header_params['X-UltraCart-Api-Version'] = @api_client.select_header_api_version()
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+      # HTTP header 'Content-Type'
+      header_params['Content-Type'] = @api_client.select_header_content_type(['application/json'])
+
+      # form parameters
+      form_params = {}
+
+      # http body (model)
+      post_body = @api_client.object_to_http_body(virtual_agent_budget)
+      auth_names = ['ultraCartOauth', 'ultraCartSimpleApiKey']
+      data, status_code, headers = @api_client.call_api(:PUT, local_var_path,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => 'ConversationVirtualAgentBudgetResponse')
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: ConversationApi#update_virtual_agent_budget\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
       end
       return data, status_code, headers
     end
